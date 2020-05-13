0ada36354d9ac08d83373f33d581a8dc72b9f7ac
Ana Maria <amreyesp@unal.edu.co>
2020-05-12 21:37:43 -0500

    [WIP] select train data method

--- aggressive_behavior_model/services/prediction_experiment.py
+++ aggressive_behavior_model/services/prediction_experiment.py
@@ -1,14 +1,15 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
-SIEDCO_DICT = {
-                'date': '',
+siedco_dict = {
+                'date': 'FECHA_HECHO',
                 'latitude': 'LATITUD_Y',
                 'longitude': 'LONGITUD_X',
-                'time': 'HORA',
-                'time_stamp':'FECHA_HECHO',
+                'time': 'HORA_HECHO',
+                'time_stamp':'',
                }
 
-NUSE_DICT = {
+nuse_dict = {
                 'date': 'FECHA',
                 'latitude': 'LATITUD',
                 'longitude': 'LONGITUD',
@@ -16,23 +17,55 @@ NUSE_DICT = {
                 'time_stamp':'',
                }
 
+DATASET_DICT = {'SIEDCO':siedco_dict, 'NUSE':nuse_dict}
+
 class predictionExperiment:
 
     def __init__(self):
-        self.db = {'name':'SIEDCO','path':''}
-        self.train_set = {'initial_date':'','final_date':''}
+        self.dataset = {'name':'SIEDCO','path':''}
+        self.train_set = {'initial_date':'2018-01-01','final_date':'2018-01-07'}
         self.metrics = {'hit-rate':[0.1],'PAI':[0.1]}
         self.validation = {'nested cross-validation':'walk-forward chaining','time_unit':'days'}
         self.model = {'type':'KDE','parameters_estimation':'custom'}
         self.aggregation_data = "subsequent"
 
+        self.dataset['data_dict'] = self.set_dictionary()
+
+    def set_dictionary(self):
+        return DATASET_DICT[self.dataset['name']].copy()
+
     def validate_exp_params(self):
         #check path exists
-        #check train set is available for defined db
+        #check initial date is previous to final date
         #check data available for validation/test (e.g., final training date is the last on the defined DB)
         pass
 
-    def select_db_data(self):
+    def select_train_data(self):
         #db is already filtered with the type of event we want to model
-        current_path = self.db['']
-        pass
+        current_path = self.dataset['path']
+        try:
+            df = pd.read_csv(current_path)
+        except FileNotFoundError:
+            return "File not found, check path and file name"
+        df = self.add_timestamp(df)
+        initial_date = self.train_set['initial_date']
+        final_date = self.train_set['final_date']
+        #print(df.columns)
+        df_filtered = self.filter_by_date(df, initial_date, final_date)
+
+        return "Successful"
+
+    def add_timestamp(self, df):
+        current_dict = self.dataset['data_dict']
+        if current_dict['time_stamp'] == '':
+            df[current_dict['date']] = pd.to_datetime(df[current_dict['date']], format='%Y-%m-%d').dt.strftime('%Y-%m-%d')
+            df['TIME_STAMP']=pd.to_datetime(df[current_dict['date']]+ ' '+df[current_dict['time']])
+            self.dataset['data_dict']['time_stamp'] = 'TIME_STAMP'
+        return df
+
+    def filter_by_date(self, df, initial_date, final_date):
+        current_dict = self.dataset['data_dict']
+        time_stamp_field = current_dict['time_stamp']
+        real_final_date = datetime.strptime(final_date,'%Y-%m-%d')+timedelta(days=1)
+        df_filtered = df[(df[time_stamp_field] > datetime.strptime(initial_date,'%Y-%m-%d')) & (df[time_stamp_field] < real_final_date)]
+        return df_filtered
--- aggressive_behavior_model/test/test_prediction_experiment.py
+++ aggressive_behavior_model/test/test_prediction_experiment.py
@@ -1,4 +1,6 @@
 import unittest
+import pandas as pd
+
 
 # Can't use from... import directly since the file is into another folder
 import os, sys
@@ -13,10 +15,49 @@ class TestCase(unittest.TestCase):
         self.my_experiment = predictionExperiment()
 
     def test_set_up(self):
-        self.my_experiment = predictionExperiment()
-        self.assertEqual(self.my_experiment.db,{'name':'SIEDCO','path':''})
-        self.assertEqual(self.my_experiment.train_set, {'initial_date':'','final_date':''})
+        siedco_dict = {
+                        'date': 'FECHA_HECHO',
+                        'latitude': 'LATITUD_Y',
+                        'longitude': 'LONGITUD_X',
+                        'time': 'HORA_HECHO',
+                        'time_stamp':'',
+                       }
+        self.assertEqual(self.my_experiment.dataset,{'name':'SIEDCO','path':'','data_dict':siedco_dict})
+        self.assertEqual(self.my_experiment.train_set, {'initial_date':'2018-01-01','final_date':'2018-01-07'})
         self.assertEqual(self.my_experiment.metrics, {'hit-rate':[0.1],'PAI':[0.1]})
         self.assertEqual(self.my_experiment.validation, {'nested cross-validation':'walk-forward chaining','time_unit':'days'})
         self.assertEqual(self.my_experiment.model, {'type':'KDE','parameters_estimation':'custom'})
         self.assertEqual(self.my_experiment.aggregation_data, 'subsequent')
+
+    def test_select_train_data_case1(self):
+        #case 1: file not found
+        head_path = '/Users/anamaria/Desktop/dev/security_project/datasets/'
+        file = 'deduplicate_siedco_10032020'
+        self.my_experiment.dataset['path'] = head_path + file
+        response = self.my_experiment.select_train_data()
+        self.assertEqual(response, "File not found, check path and file name")
+
+    def test_select_train_data_case2(self):
+        #case 2: file found
+        head_path = '/Users/anamaria/Desktop/dev/security_project/datasets/'
+        file = 'deduplicate_siedco_10032020.csv'
+        self.my_experiment.dataset['path'] = head_path + file
+        response = self.my_experiment.select_train_data()
+        self.assertNotEqual(response, "File not found, check path and file name")
+
+    def test_filter_by_date_case1(self):
+        #case 1: date on interval, siedco
+        head_path = '/Users/anamaria/Desktop/dev/security_project/datasets/'
+        file = 'deduplicate_siedco_10032020.csv'
+        df = pd.read_csv(head_path + file)
+        df = self.my_experiment.add_timestamp(df)
+        initial_date = self.my_experiment.train_set['initial_date']
+        final_date = self.my_experiment.train_set['final_date']
+        df_filtered = self.my_experiment.filter_by_date(df,initial_date,final_date)
+
+        mask = (df['TIME_STAMP'] > initial_date) & (df['TIME_STAMP'] <= final_date)
+        df_expected = df.loc[mask]
+        print(df_expected.TIME_STAMP)
+        print(df_filtered.TIME_STAMP)
+
+        self.assertEqual(df_filtered,df_expected)
