e994b2ec544c73082e3048b2b6abd5388739b2f0
Ana Maria <amreyesp@unal.edu.co>
2020-05-28 19:08:35 -0500

    [WIP] include validation_dates arguments on validation methods

--- aggressive_behavior_model/services/validate_model.py
+++ aggressive_behavior_model/services/validate_model.py
@@ -8,8 +8,8 @@ from services.aggressive_model import NaiveCounting, SpaceTimeKDE, SEPPexp
 
 class ValidateModel:
 
-    def __init__(self, df_train, dataset_dict, time_unit, outer_iterations):
-        self.df_train = df_train
+    def __init__(self, df_train_validation, dataset_dict, time_unit, outer_iterations):
+        self.df_train_validation = df_train_validation
         self.dataset_dict = dataset_dict
         self.time_unit = time_unit
         self.outer_iterations = outer_iterations
@@ -20,7 +20,8 @@ class ValidateModel:
 
     def inner_loop_validation(self, model_class_name, grid_size, train_subset_dates, current_validation_date, metrics):
         model_object = globals()[model_class_name]()
-        df_train_subset = ProcessData.filter_by_date(self.df_train, self.dataset_dict, train_subset_dates['initial'], train_subset_dates['final'])
+        df_train_subset = ProcessData.filter_by_date(self.df_train_validation, self.dataset_dict, train_subset_dates['initial'], train_subset_dates['final'])
+        print(len(df_train_subset))
         trained_model = model_object.train(df_train_subset, self.dataset_dict, grid_size)
         ## TODO: save trained_model
 
@@ -31,21 +32,26 @@ class ValidateModel:
         area_rates = list(map(lambda a: a*hotspot_percentage/100, area_rates))
 
         validation_dates = {'initial':current_validation_date,'final':current_validation_date}
-        df_validation = ProcessData.filter_by_date(self.df_train, self.dataset_dict, validation_dates['initial'], validation_dates['final'])
-        validation_points, validation_region = ProcessData.get_time_space_points(df_validation, self.dataset_dict)
+        df_validation = ProcessData.filter_by_date(self.df_train_validation, self.dataset_dict, validation_dates['initial'], validation_dates['final'])
+        try:
+            validation_points, validation_region = ProcessData.get_time_space_points(df_validation, self.dataset_dict)
+        except: #if no points (e.g. crimes) are not reported on data interval
+            pass
         prediction_date = current_validation_date
         flag_performance_array = True
 
         for hour in range(0, 24, delta_hour):
-            time_predicts = np.datetime64(prediction_date) + np.timedelta64(hour, 'h')
-            score_end_time = time_predicts + np.timedelta64(delta_hour, 'h')
-            mask = (validation_points.timestamps >= time_predicts) & (validation_points.timestamps < score_end_time)
-            eval_pts = validation_points[mask]
-
             prediction_datetime = prediction_date+timedelta(hours=hour)
-            prediction = model_object.predict(trained_model, prediction_datetime)
+            if df_validation.empty: #if no points (e.g. crimes) are not reported on data interval
+                hitrates = { i : -1.0 for i in area_rates }
+            else:
+                time_predicts = np.datetime64(prediction_date) + np.timedelta64(hour, 'h')
+                score_end_time = time_predicts + np.timedelta64(delta_hour, 'h')
+                mask = (validation_points.timestamps >= time_predicts) & (validation_points.timestamps < score_end_time)
+                eval_pts = validation_points[mask]
+                prediction = model_object.predict(trained_model, prediction_datetime)
+                hitrates = open_cp.evaluation.hit_rates(prediction, eval_pts, area_rates)
 
-            hitrates = open_cp.evaluation.hit_rates(prediction, eval_pts, area_rates)
             if flag_performance_array==True:
                 flag_performance_array = False
                 performance_metrics = np.array([prediction_datetime,hitrates]);
@@ -61,13 +67,17 @@ class ValidateModel:
         current_validation_date = current_validation_date + timedelta(days=1)
         return (train_subset_dates, current_validation_date)
 
-    def walk_fwd_chain(self, model_class_name, grid_size, dates, metrics):
-        initial_date = datetime.strptime(dates['initial'],'%Y-%m-%d')
-        final_date = datetime.strptime(dates['final'],'%Y-%m-%d')
-        current_validation_date = initial_date+timedelta(days=1)
-        train_subset_dates = {'initial':initial_date,'final':initial_date}
+    def walk_fwd_chain(self, model_class_name, grid_size, train_dates, validation_dates, metrics):
+        initial_train_date = datetime.strptime(train_dates['initial'],'%Y-%m-%d')
+        final_train_date = datetime.strptime(train_dates['final'],'%Y-%m-%d')
+        initial_validation_date = datetime.strptime(validation_dates['initial'],'%Y-%m-%d')
+        final_validation_date = datetime.strptime(validation_dates['final'],'%Y-%m-%d')
+        current_validation_date = initial_validation_date
+        train_subset_dates = {'initial':initial_train_date,'final':final_train_date}
         performance_array = []
-        while current_validation_date <= final_date:
+        while current_validation_date <= final_validation_date:
+            print('train_subset_dates', train_subset_dates)
+            print('current_validation_date', current_validation_date)
             performance_metrics = self.inner_loop_validation(model_class_name, grid_size, train_subset_dates, current_validation_date, metrics)
             performance_array.append(performance_metrics)
             train_subset_dates, current_validation_date = ValidateModel.update_train_validation_subsets(train_subset_dates,current_validation_date)
