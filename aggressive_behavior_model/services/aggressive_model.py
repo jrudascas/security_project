c73af5f0195319af7f83ff67530b7da9ab84eccc
Ana Maria <amreyesp@unal.edu.co>
2020-06-22 14:37:30 -0500

    sepp model based on week-day

--- aggressive_behavior_model/services/aggressive_model.py
+++ aggressive_behavior_model/services/aggressive_model.py
@@ -17,30 +17,50 @@ class AggressiveModel:
 
 class NaiveCounting(AggressiveModel):
 
-    def train(self, df_train_subset, dataset_dict, grid_size):
-        training_points, training_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
-        trained_model = naive.CountingGridKernel(grid_width=grid_size, region=training_region)
-        trained_model.data = training_points
+    def train(self, df_train_subset, dataset_dict, grid_size, **kwargs):
+        train_pts, train_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
+        trained_model = naive.CountingGridKernel(grid_width=grid_size, region=train_region)
+        trained_model.data = train_pts
         return trained_model
 
 class SpaceTimeKDE(AggressiveModel):
 
-    def train(self, df_train_subset, dataset_dict, grid_size):
-        training_points, training_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
-        trained_model = kde.KDE(region=training_region, grid_size=grid_size)
+    def train(self, df_train_subset, dataset_dict, grid_size, **kwargs):
+        train_pts, train_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
+        trained_model = kde.KDE(region=train_region, grid_size=grid_size)
         trained_model.time_kernel = kde.ExponentialTimeKernel(1)
         trained_model.space_kernel = kde.GaussianBaseProvider()
-        trained_model.data = training_points
+        trained_model.data = train_pts
         return trained_model
 
 class SEPPexp(AggressiveModel):
 
-    def train(self, df_train_subset, dataset_dict, grid_size):
-        training_points, training_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
-        trainer = seppexp.SEPPTrainer(region=training_region, grid_size=grid_size)
-        trainer.data = training_points
-        trained_model = trainer.train(iterations=100, use_corrected=True)
-        trained_model.data = training_points
+    def train(self, df_train_subset, dataset_dict, grid_size, **kwargs):
+        train_pts, train_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
+        trainer = seppexp.SEPPTrainer(region=train_region, grid_size=grid_size)
+        trainer.data = train_pts
+        trained_model = trainer.train(iterations=50, use_corrected=True)
+        trained_model.data = train_pts
+        return trained_model
+
+    def predict(self, trained_model, prediction_datetime):
+        return trained_model.predict(prediction_datetime)
+
+class SEPPexpWeekDay(AggressiveModel):
+    """This model is trained according to the week-day that will be predicted.
+    Just historical data of same week-day is selected to train the model.
+    """
+    def train(self, df_train_subset, dataset_dict, grid_size, **kwargs):
+        df_train_subset['weekday'] = df_train_subset['TIME_STAMP'].dt.day_name()
+        df_train_subset = ProcessData.filter_by_field(df_train_subset,
+                                                      'weekday',
+                                                      kwargs['week_day'])
+
+        train_pts, train_region = ProcessData.get_time_space_points(df_train_subset, dataset_dict)
+        trainer = seppexp.SEPPTrainer(region=train_region, grid_size=grid_size)
+        trainer.data = train_pts
+        trained_model = trainer.train(iterations=50, use_corrected=True)
+        trained_model.data = train_pts
         return trained_model
 
     def predict(self, trained_model, prediction_datetime):
@@ -48,6 +68,6 @@ class SEPPexp(AggressiveModel):
 
 class SEPPCov(AggressiveModel):
     # TODO:
-    def train(self, df_train_subset, dataset_dict, grid_size):
+    def train(self, df_train_subset, dataset_dict, grid_size, **kwargs):
         #should return fitted model
         pass
