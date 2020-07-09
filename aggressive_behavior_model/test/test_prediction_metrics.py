41c521986d307edfe88e21ae2b834b8862146602
Ana Maria <amreyesp@unal.edu.co>
2020-07-08 22:40:02 -0500

    [WIP] MSE measure tests

--- aggressive_behavior_model/test/test_prediction_metrics.py
+++ aggressive_behavior_model/test/test_prediction_metrics.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 import pandas as pd
 import pickle
 
@@ -16,10 +17,28 @@ class TestCase(unittest.TestCase):
     def SetUp(self):
         pass
 
-    def test_hit_rate_default(self):
+    def test_hit_rate_default_1(self):
+        """ Test hit_rate=1 if all real events falls on hotspots """
         pass
 
-    def test_hit_rate_ground_truth(self):
+    def test_hit_rate_default_2(self):
+        """ Test hit_rate=0 if no events falls on hotspots """
+        pass
+
+    def test_hit_rate_default_3(self):
+        """ Test based on Candelaria scenario """
+        pass
+
+    def test_hit_rate_ground_truth_1(self):
+        """ Test hit_rate=1 if all real events falls on hotspots """
+        pass
+
+    def test_hit_rate_ground_truth_2(self):
+        """ Test hit_rate=0 if no events falls on hotspots """
+        pass
+
+    def test_hit_rate_ground_truth_3(self):
+        """ Test based on Candelaria scenario """
         pass
 
     def test_make_counting_grid(self):
@@ -44,3 +63,24 @@ class TestCase(unittest.TestCase):
         self.assertEqual(counting_matrix._matrix.shape, (816, 343))
         self.assertEqual(counting_matrix._matrix.max(),357)
         self.assertEqual(counting_matrix._matrix.min(),0)
+
+    def test_mse_match(self):
+        """ Test mse results match using different methods"""
+        infile = open('/Users/anamaria/Desktop/dev/security_project/aggressive_behavior_model/pkl/experiment_seppexp_10_2_siedco_prediction.pkl','rb')
+        loaded_siedco = pickle.load(infile)
+        infile.close()
+        grid = loaded_siedco['prediction'].values[0]
+        grid._matrix = ProcessData.normalize_matrix(grid._matrix)
+        real_events = loaded_siedco['eval_pts'].values[0]
+        mse_method_1 = prediction_metrics.mse(grid,real_events)
+
+        counting_matrix = prediction_metrics.make_counting_grid(grid, real_events)
+        counting_matrix._matrix = ProcessData.normalize_matrix(counting_matrix._matrix)
+        mse_method_2 = np.sum((grid._matrix.astype("float") - counting_matrix._matrix.astype("float")) ** 2)
+        mse_method_2 /= float(grid._matrix.shape[0] * grid._matrix.shape[1])
+
+        self.assertEqual(mse_method_1, mse_method_2)
+
+    def test_mse_1(self):
+        """ Test mse=0 if both matrices are equal """
+        pass
